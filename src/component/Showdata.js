d24f3ecbfe26fca3776c82cd5c0e7db07a516841
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2022-02-19 20:33:44 +0530

    push on githug

--- src/component/Showdata.js
+++ src/component/Showdata.js
@@ -6,6 +6,7 @@ export default function Showdata(props) {
             <div className="card my-4">
                 <div className="card-header">{props.stateName}</div>
                 <ul className="list-group list-group-flush">
+                
                     <li className="list-group-item">Total Casese {props.confirmed}</li>
                     <li className="list-group-item">Total Active Cases {props.active}</li>
                     <li className="list-group-item">Total Recovered {props.recovered}</li>
